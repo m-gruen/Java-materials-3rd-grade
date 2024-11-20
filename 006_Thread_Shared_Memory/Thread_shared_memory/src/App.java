6af0107d3557ea3b12ce7d16693962732fd8739a
Mark Grünzweil <mark.gruenzweil@gmail.com>
2024-11-20 12:30:18 +0100

    Finished 006_Shared_Memory

--- 006_Thread_Shared_Memory/Thread_shared_memory/src/App.java
+++ 006_Thread_Shared_Memory/Thread_shared_memory/src/App.java
@@ -1,20 +1,44 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        StopWatch stopWatch = new StopWatch();
+
+        Thread green = new Thread(
+            () -> stopWatch.countDown(7),
+            ThreadColor.ANSI_GREEN.name()
+        );
+
+        Thread red = new Thread(
+            () -> stopWatch.countDown(7),
+            ThreadColor.ANSI_RED.name()
+        );
+
+        Thread blue = new Thread(
+            () -> stopWatch.countDown(5),
+            ThreadColor.ANSI_BLUE.name()
+        );
+
+        green.start();
+        red.start();
+        blue.start();
     }
 }
 
 class StopWatch {
-    public StopWatch() {
+    // private int i; // This is not a good idea
 
+    public StopWatch() {
     }
 
     public void countDown(int startCount) {
 
         String threadName = Thread.currentThread().getName();
-        ThreadColor threadColor = ThreadColor.ANSI_GREEN;
+        ThreadColor threadColor = ThreadColor.ANSI_RESET;
+        try {
+            threadColor = ThreadColor.valueOf(threadName);
+        } catch (IllegalArgumentException e){ // Gets ignored
+        }
+
         String color = threadColor.color();
-        
 
         for (int i = startCount; i > 0; i--) {
             try {
