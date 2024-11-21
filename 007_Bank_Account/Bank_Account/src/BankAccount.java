93bdde3dd56f0ca6c09326203f5c4c27354a9d6b
Mark Grünzweil <mark.gruenzweil@gmail.com>
2024-11-21 13:21:59 +0100

    Implement BankAccount class with deposit and withdraw methods; add Main class for concurrent transactions demonstration (not good)

--- 007_Bank_Account/Bank_Account/src/BankAccount.java
+++ 007_Bank_Account/Bank_Account/src/BankAccount.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.TimeUnit;
+
 public class BankAccount {
     private double balance;
 
@@ -5,11 +7,51 @@ public class BankAccount {
         this.balance = balance;
     }
 
-    public void deposit(double amount) {
+    public double getBalance() {
+        return balance;
+    }
 
+    public void deposit(double amount) {
+        try {
+            System.out.println("Einzahlung: Warten auf System...");
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        double originalBalance = balance;
+
+        balance += amount;
+        System.out.printf(
+                "Alter Kontostand: %.2f, Einzahlung: %.2f, Neuer Kontostand: %.2f\n",
+                originalBalance,
+                amount,
+                balance);
     }
 
     public void withdraw(double amount) {
+        try {
+            System.out.println("Auszahlung: Warten auf System...");
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        double originalBalance = balance;
+
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.printf(
+                    "Alter Kontostand: %.2f, Auszahlung: %.2f, Neuer Kontostand: %.2f\n",
+                    originalBalance,
+                    amount,
+                    balance);
+        } else {
+            System.out.printf(
+                    "Alter Kontostand: %.2f, Einzahlung: %.2f, Guthaben nicht ausreichend\n",
+                    originalBalance,
+                    amount);
+        }
 
     }
 }
