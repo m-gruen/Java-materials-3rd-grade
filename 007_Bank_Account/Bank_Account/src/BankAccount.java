d423cbe6c2523b40d2a54643dee678827aec3287
Mark Grünzweil <mark.gruenzweil@gmail.com>
2024-11-21 13:49:42 +0100

    Refactor deposit and withdraw methods to use synchronized blocks for thread safety

--- 007_Bank_Account/Bank_Account/src/BankAccount.java
+++ 007_Bank_Account/Bank_Account/src/BankAccount.java
@@ -11,7 +11,7 @@ public class BankAccount {
         return balance;
     }
 
-    public synchronized void deposit(double amount) {
+    public void deposit(double amount) {
         try {
             System.out.println("Einzahlung: Warten auf System...");
             TimeUnit.SECONDS.sleep(5);
@@ -19,17 +19,18 @@ public class BankAccount {
             throw new RuntimeException(e);
         }
 
-        double originalBalance = balance;
-
-        balance += amount;
-        System.out.printf(
-                "Alter Kontostand: %.2f, Einzahlung: %.2f, Neuer Kontostand: %.2f\n",
-                originalBalance,
-                amount,
-                balance);
+        synchronized (this) {
+            double originalBalance = balance;
+            balance += amount;
+            System.out.printf(
+                    "Alter Kontostand: %.2f, Einzahlung: %.2f, Neuer Kontostand: %.2f\n",
+                    originalBalance,
+                    amount,
+                    balance);
+        }
     }
 
-    public synchronized void withdraw(double amount) {
+    public void withdraw(double amount) {
         try {
             System.out.println("Auszahlung: Warten auf System...");
             TimeUnit.SECONDS.sleep(1);
@@ -37,21 +38,21 @@ public class BankAccount {
             throw new RuntimeException(e);
         }
 
-        double originalBalance = balance;
-
-        if (amount <= balance) {
-            balance -= amount;
-            System.out.printf(
-                    "Alter Kontostand: %.2f, Auszahlung: %.2f, Neuer Kontostand: %.2f\n",
-                    originalBalance,
-                    amount,
-                    balance);
-        } else {
-            System.out.printf(
-                    "Alter Kontostand: %.2f, Einzahlung: %.2f, Guthaben nicht ausreichend\n",
-                    originalBalance,
-                    amount);
+        synchronized (this) {
+            double originalBalance = balance;
+            if (amount <= balance) {
+                balance -= amount;
+                System.out.printf(
+                        "Alter Kontostand: %.2f, Auszahlung: %.2f, Neuer Kontostand: %.2f\n",
+                        originalBalance,
+                        amount,
+                        balance);
+            } else {
+                System.out.printf(
+                        "Alter Kontostand: %.2f, Einzahlung: %.2f, Guthaben nicht ausreichend\n",
+                        originalBalance,
+                        amount);
+            }
         }
-
     }
 }
