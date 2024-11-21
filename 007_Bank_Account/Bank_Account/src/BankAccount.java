0e03ee5fc913f36b555ee4b7a1485ba954f8e760
Mark Grünzweil <mark.gruenzweil@gmail.com>
2024-11-21 12:59:04 +0100

    Added new projekt: 007_Bank_Account and implemented the first lines

---
+++ 007_Bank_Account/Bank_Account/src/BankAccount.java
@@ -0,0 +1,15 @@
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+
+    }
+
+    public void withdraw(double amount) {
+
+    }
+}
