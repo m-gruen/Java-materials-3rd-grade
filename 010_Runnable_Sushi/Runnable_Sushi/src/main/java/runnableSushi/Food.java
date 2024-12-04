adfeafedf9b6239677afd21e72e91170de26ae5d
Mark Grünzweil <mark.gruenzweil@gmail.com>
2024-12-04 11:05:58 +0100

    Add initial implementation of Runnable Sushi simulation with Food, Consumer, Producer, and Belt classes

---
+++ 010_Runnable_Sushi/Runnable_Sushi/src/main/java/runnableSushi/Food.java
@@ -0,0 +1,35 @@
+package runnableSushi;
+
+public class Food {
+
+    private String id;
+    private FoodType foodType;
+
+    /**
+     * Constructor
+     * @param id food id
+     * @param foodType food type
+     */
+    public Food(String id, FoodType foodType) {
+        // TODO
+    }
+
+    /**
+     * Get food id
+     * @return food id
+     */
+    public String getId() {
+        // TODO
+        return "";
+    }
+
+    /**
+     * A string representation of the food
+     * @return string representation of the food
+     */
+    @Override
+    public String toString() {
+        // TODO
+        return "";
+    }
+}
