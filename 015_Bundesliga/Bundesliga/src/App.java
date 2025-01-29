c997070b9d2fb1905ef981a40252784243df4234
Mark Grünzweil <mark.gruenzweil@gmail.com>
2025-01-29 11:57:13 +0100

    Implement match data reading from CSV file in App class

--- 015_Bundesliga/Bundesliga/src/App.java
+++ 015_Bundesliga/Bundesliga/src/App.java
@@ -1,5 +1,19 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        
+        List<Match> file = null;
+        try {
+            file = FileReader.readMatchesFromFile(Path.of("files/bundesliga_23_24.csv"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (file != null) {
+            file.forEach(System.out::println);
+        }
     }
 }
