10a28778c4c2b5d7549d8c3eeecedc6b74e51fc3
Mark Grünzweil <mark.gruenzweil@gmail.com>
2025-02-05 11:46:53 +0100

    Implement basic layout with buttons in HelloApplication class

--- 016_Java_Fx/JavaFx_intro/src/main/java/com/example/javafx_intro/HelloApplication.java
+++ 016_Java_Fx/JavaFx_intro/src/main/java/com/example/javafx_intro/HelloApplication.java
@@ -2,7 +2,10 @@ package com.example.javafx_intro;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +13,22 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        HBox root = new HBox();
+        root.setPrefSize(340, 70);
+        root.setSpacing(10);
+        root.setAlignment(Pos.CENTER);
+
+        // Buttons anlegen
+        Button btnUntis = new Button();
+        Button btnMoodle = new Button();
+        Button btnMail = new Button();
+        Button btnWiki = new Button();
+        Button btnExit = new Button();
+
+        root.getChildren().addAll(btnUntis, btnMoodle, btnMail, btnWiki, btnExit);
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
