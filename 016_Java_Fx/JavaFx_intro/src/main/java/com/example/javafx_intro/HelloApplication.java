4f0036a712db85aa3f59cd79be44af104b6f9c9a
Mark Grünzweil <mark.gruenzweil@gmail.com>
2025-02-12 09:43:02 +0100

    Add action handlers and styles for buttons in HelloApplication

--- 016_Java_Fx/JavaFx_intro/src/main/java/com/example/javafx_intro/HelloApplication.java
+++ 016_Java_Fx/JavaFx_intro/src/main/java/com/example/javafx_intro/HelloApplication.java
@@ -1,6 +1,8 @@
 package com.example.javafx_intro;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -25,7 +27,33 @@ public class HelloApplication extends Application {
         Button btnWiki = new Button();
         Button btnExit = new Button();
 
+        btnUntis.setPrefSize(64, 64);
+        btnMoodle.setPrefSize(64, 64);
+        btnMail.setPrefSize(64, 64);
+        btnWiki.setPrefSize(64, 64);
+        btnExit.setPrefSize(64, 64);
+
+        btnUntis.getStyleClass().add("btnUntis");
+        btnMoodle.getStyleClass().add("btnMoodle");
+        btnMail.getStyleClass().add("btnMail");
+        btnWiki.getStyleClass().add("btnWiki");
+        btnExit.getStyleClass().add("btnExit");
+
         root.getChildren().addAll(btnUntis, btnMoodle, btnMail, btnWiki, btnExit);
+
+        btnUntis.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                getHostServices().showDocument("https://mese.webuntis.com/WebUntis/?school=htbla%20linz%20leonding#/basic/login");
+            }
+        });
+
+        btnMoodle.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                getHostServices().showDocument("https://edufs.edu.htl-leonding.ac.at/moodle/");
+            }
+        });
 
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
