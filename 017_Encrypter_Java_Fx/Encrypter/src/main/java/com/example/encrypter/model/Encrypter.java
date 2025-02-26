6ab6536c03449312812af9342be9090eb0969b19
Mark Grünzweil <mark.gruenzweil@gmail.com>
2025-02-26 11:44:39 +0100

    Add Encrypter class with basic structure and placeholder methods

---
+++ 017_Encrypter_Java_Fx/Encrypter/src/main/java/com/example/encrypter/model/Encrypter.java
@@ -0,0 +1,24 @@
+package com.example.encrypter.model;
+
+public class Encrypter {
+
+    private String alphabet;
+    private String key;
+    private String plainText;
+    private String encryptedText;
+
+    public Encrypter() {
+
+    }
+
+    // Getter and Setter ????
+
+    // Methoden
+    public void generateRandomKey() {
+
+    }
+
+    private void encrypt() {
+
+    }
+}
