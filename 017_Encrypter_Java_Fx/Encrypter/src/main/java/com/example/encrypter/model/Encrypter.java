82fdd6f8a9b91e22c003531b6e0fd204d72ef863
Mark Grünzweil <mark.gruenzweil@gmail.com>
2025-02-27 13:55:21 +0100

    Refactor key generation and implement save functionality in Encrypter class

--- 017_Encrypter_Java_Fx/Encrypter/src/main/java/com/example/encrypter/model/Encrypter.java
+++ 017_Encrypter_Java_Fx/Encrypter/src/main/java/com/example/encrypter/model/Encrypter.java
@@ -3,6 +3,7 @@ package com.example.encrypter.model;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
+import java.util.Locale;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -78,12 +79,15 @@ public class Encrypter {
     }
 
     public void generateRandomKey() {
-        String alphabet = getAlphabet();
+        String distinctAlphabet = getAlphabet().chars()
+                .distinct()
+                .mapToObj(c -> String.valueOf((char)c))
+                .collect(Collectors.joining());
 
         Random random = new Random();
-        String key = Stream.generate(() -> alphabet.charAt(random.nextInt(alphabet.length())))
+        String key = Stream.generate(() -> distinctAlphabet.charAt(random.nextInt(distinctAlphabet.length())))
                 .distinct()
-                .limit(alphabet.length())
+                .limit(distinctAlphabet.length())
                 .map(String::valueOf)
                 .collect(Collectors.joining());
 
@@ -91,16 +95,25 @@ public class Encrypter {
     }
 
     private void encrypt() {
-        // TODO: Implement encryption
         String alphabet = getAlphabet();
+        String key = getKey();
+        String plainText = getPlainText().toLowerCase();
 
-        Random random = new Random();
-        String key = Stream.generate(() -> alphabet.charAt(random.nextInt(alphabet.length())))
-                .distinct()
-                .limit(alphabet.length())
+        String encryptedText = plainText.chars()
+                .mapToObj(c -> (char) c)
+                .map(c -> {
+                    int index = alphabet.indexOf(c);
+                    return index >= 0 ? key.charAt(index) : c;
+                })
                 .map(String::valueOf)
                 .collect(Collectors.joining());
 
-        setEncryptedText(key);
+        setEncryptedText(encryptedText);
+    }
+
+    public void saveData() {
+        FileWriterUtil.writeToFile(getEncryptedText(), "encrypted.txt");
+        FileWriterUtil.writeToFile(getPlainText(), "plainText.txt");
+        FileWriterUtil.writeToFile(getAlphabet() + "\n" + getKey(), "alphabetAndKey.txt");
     }
 }
