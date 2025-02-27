a80e6e6e39c97ec6ab22ea12ef82a1e7ef0491db
Mark Grünzweil <mark.gruenzweil@gmail.com>
2025-02-27 12:53:02 +0100

    Enhance Encrypter class with properties for alphabet, key, plainText, and encryptedText; add methods for setting defaults and generating random keys

--- 017_Encrypter_Java_Fx/Encrypter/src/main/java/com/example/encrypter/model/Encrypter.java
+++ 017_Encrypter_Java_Fx/Encrypter/src/main/java/com/example/encrypter/model/Encrypter.java
@@ -1,24 +1,104 @@
 package com.example.encrypter.model;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Encrypter {
 
-    private String alphabet;
-    private String key;
-    private String plainText;
-    private String encryptedText;
+    private final String DEFAULT_ALPHABET = "abcdefghijklmnopqrstuvwxyzäöüß";
+    private StringProperty alphabet = new SimpleStringProperty("");
+    private StringProperty key = new SimpleStringProperty("");
+    private StringProperty plainText = new SimpleStringProperty("");
+    private StringProperty encryptedText = new SimpleStringProperty("");
 
     public Encrypter() {
-
+        setDefaultAlphabet();
+        setPlainText("Please enter your text here.");
     }
 
     // Getter and Setter ????
 
-    // Methoden
-    public void generateRandomKey() {
+    public String getAlphabet() {
+        return alphabet.get();
+    }
 
+    public StringProperty alphabetProperty() {
+        return alphabet;
+    }
+
+    public String getKey() {
+        return key.get();
+    }
+
+    public StringProperty keyProperty() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key.set(key);
+    }
+
+    public String getPlainText() {
+        return plainText.get();
+    }
+
+    public StringProperty plainTextProperty() {
+        return plainText;
+    }
+
+    public void setPlainText(String plainText) {
+        this.plainText.set(plainText);
+    }
+
+    public String getEncryptedText() {
+        return encryptedText.get();
+    }
+
+    public StringProperty encryptedTextProperty() {
+        return encryptedText;
+    }
+
+    public void setEncryptedText(String encryptedText) {
+        this.encryptedText.set(encryptedText);
+    }
+
+    public void setAlphabet(String alphabet) {
+        this.alphabet.set(alphabet);
+    }
+
+    // Methoden
+    public void setDefaultAlphabet() {
+        setAlphabet(DEFAULT_ALPHABET);
+    }
+
+    public void generateRandomKey() {
+        String alphabet = getAlphabet();
+
+        Random random = new Random();
+        String key = Stream.generate(() -> alphabet.charAt(random.nextInt(alphabet.length())))
+                .distinct()
+                .limit(alphabet.length())
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+
+        setKey(key);
     }
 
     private void encrypt() {
+        // TODO: Implement encryption
+        String alphabet = getAlphabet();
 
+        Random random = new Random();
+        String key = Stream.generate(() -> alphabet.charAt(random.nextInt(alphabet.length())))
+                .distinct()
+                .limit(alphabet.length())
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+
+        setEncryptedText(key);
     }
 }
