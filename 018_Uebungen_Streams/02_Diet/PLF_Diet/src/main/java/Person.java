86bdc8551a5b660393908a8cd3d4a2fd79ec1089
Mark Grünzweil <mark.gruenzweil@gmail.com>
2025-03-05 12:40:46 +0100

    Implement Person class methods for age calculation, BMI computation, and BMI classification

--- 018_Uebungen_Streams/02_Diet/PLF_Diet/src/main/java/Person.java
+++ 018_Uebungen_Streams/02_Diet/PLF_Diet/src/main/java/Person.java
@@ -14,27 +14,50 @@ public record Person(
 
     public String toString() {
         // TODO
-        return "";
+        return String.format("%s %d\t%-30s %s %s (%d) BMI %.1f-%-15s\t%-4d\t%5.1f\t|\t%5.1f (%s%.1f)",
+                hasAborted() ? "*" : " ",
+                id,
+                lastname + " " + firstname,
+                gender,
+                dayOfBirth.toString(),
+                getAge(),
+                getBMI(),
+                getBMIClass(),
+                height,
+                weight_T1,
+                weight_T2,
+                weight_T2 < weight_T1 ? "-" : "+",
+                Math.abs(weight_T2 - weight_T1));
     }
 
     public boolean hasAborted() {
         // TODO
-        return false;
+        return weight_T2 == 0;
     }
 
     public int getAge() {
         // TODO
-        return -1;
+        return LocalDate.now().getYear() - dayOfBirth.getYear();
     }
 
     public double getBMI() {
         // TODO
-        return -1;
+        return weight_T1 / ((double) height / 100d * (double) height / 100);
     }
 
     public String getBMIClass() {
         // TODO
-        return "";
+        double bmi = getBMI();
+
+        if (bmi < 18.5) {
+            return "underweight";
+        } else if (bmi < 25.0) {
+            return "normal weight";
+        } else if (bmi < 30.0) {
+            return "overweight";
+        } else {
+            return "obese";
+        }
     }
 
 }
