c44c48ff459984952ae7c6b5b9ef7a4f692ef2fb
Mark Grünzweil <mark.gruenzweil@gmail.com>
2025-03-05 16:03:03 +0100

    Implement file reading and processing for Person objects

--- 018_Uebungen_Streams/02_Diet/PLF_Diet/src/main/java/FileReader.java
+++ 018_Uebungen_Streams/02_Diet/PLF_Diet/src/main/java/FileReader.java
@@ -11,12 +11,36 @@ import java.util.Locale;
 public class FileReader {
 
     public static List<Person> readPersonsFromFile(Path filePath) {
-       // TODO
-        return null;
+        List<Person> persons = new ArrayList<>();
+        try {
+            persons = Files.readAllLines(filePath).stream()
+                    .skip(1)
+                    .map(FileReader::processLine)
+                    .toList();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return persons;
     }
 
     private static Person processLine(String line) {
         // TODO
+        try {
+            String[] parts = line.split(";");
+            int id = Integer.parseInt(parts[0]);
+            String firstName = parts[1];
+            String lastName = parts[2];
+            char gender = parts[3].charAt(0);
+            LocalDate dayOfBirth = LocalDate.parse(parts[4], DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+            int height = Integer.parseInt(parts[5]);
+            double weight_T1 = Double.parseDouble(parts[6]);
+            double weight_T2 = Double.parseDouble(parts[7]);
+
+            return new Person(id, firstName, lastName, gender, dayOfBirth, height, weight_T1, weight_T2);
+        } catch (Exception e) {
+            System.out.println("Error while processing line: " + line);
+        }
+
         return null;
     }
 
