5c18e6b708ac37d53b58ffd595c448edc77684a9
Mark Grünzweil <mark.gruenzweil@gmail.com>
2025-03-20 14:29:09 +0100

    Refactor ContactManagerApp to extend Application and integrate ContactPresenter for UI initialization

--- 019_Contact_Manager/contact_manager/src/main/java/com/example/contact_manager/ContactManagerApp.java
+++ 019_Contact_Manager/contact_manager/src/main/java/com/example/contact_manager/ContactManagerApp.java
@@ -1,4 +1,17 @@
 package com.example.contact_manager;
 
-public class ContactManagerApp {
+import com.example.contact_manager.view.ContactPresenter;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ContactManagerApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        ContactPresenter.show(primaryStage);
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
